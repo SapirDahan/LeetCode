2186d9ca483e6a69d4bc04090fc9e759392db3d8
Sapir Dahan <118118778+SapirDahan@users.noreply.github.com>
2024-08-27 15:41:44 +0300

    Time: 41 ms (77.62%) | Memory: 17.3 MB (41.08%) - LeetSync

--- my_solutions/125-valid-palindrome/valid-palindrome.py
+++ my_solutions/125-valid-palindrome/valid-palindrome.py
@@ -1,18 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        str_original = ""
-        str_reversed = ""
-
-        # Append the chars accordingly in the new strings
-        for i in range(0, len(s)):
-            if 'a' <= s[i] <= 'z' or 'A' <= s[i] <= 'Z' or '0' <= s[i] <= '9':
-                str_original += s[i].lower()
-                str_reversed = s[i].lower() + str_reversed
-
-        if str_original == str_reversed:
-            return True
+        # Clean the string by removing non-alphabetic characters and converting it to lowercase
+        cleaned = ''.join([char for char in s if char.isalnum()]).lower()
         
-        return False
-
-        
+        # Check if the cleaned string is equal to its reverse
+        return cleaned == cleaned[::-1]
